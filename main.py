d95685562c56493adf07b8a07280ee759403804b
Andrian <lutsu.andrian4ik96@gmail.com>
2020-09-30 22:58:20 +0300

    interface done, but problems with ascii

--- main.py
+++ main.py
@@ -2,6 +2,11 @@
 # extended_ASCII = ''.join(chr(x) for x in range(256))
 # dictionary = {i: chr(i) for i in range(128)}
 # alphabet = {i: ord(i) for i in ASCII}
+import sys
+import random
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from interface import Ui_vigenere
 
 
 class Vigenere:
@@ -10,6 +15,7 @@ class Vigenere:
     cipher = []
     original = []
     bit = 128
+    keys = 0
 
     def __init__(self, text='', key='', cipher='', bit=128):
         self.text = text
@@ -17,6 +23,7 @@ class Vigenere:
         self.original = list(text)
         self.cipher = list(cipher)
         self.bit = bit
+        self.keys = len(key)
 
     def generate_key(self, text):
         self.key = list(self.key)
@@ -52,3 +59,44 @@ class Vigenere:
             for i in range(len(self.cipher)):
                 self.original.append(chr((ord(self.cipher[i]) - ord(self.key[i]) + self.bit) % self.bit))
             return "".join(self.original)
+
+    def possible_keys(self):
+        return self.bit ** self.keys + self.bit
+
+
+# Create app
+app = QtWidgets.QApplication(sys.argv)
+# init
+vigenere = QtWidgets.QMainWindow()
+
+ui = Ui_vigenere()
+ui.setupUi(vigenere)
+vigenere.show()
+
+# logic
+message = Vigenere(key='World')
+
+
+def update_label(value):
+    message.bit = value
+    ui.bit_lbl.setText(str(value))
+    return ui.cipher.setText(message.encrypt())
+
+
+ui.bit_slider.valueChanged.connect(update_label)
+
+
+def set_key():
+    message.key = ui.key.toPlainText()
+    return ui.cipher.setText(message.encrypt())
+
+
+def set_text():
+    message.text = ui.text.toPlainText()
+    return ui.cipher.setText(message.encrypt())
+
+
+ui.key.textChanged.connect(set_key)
+ui.text.textChanged.connect(set_text)
+
+sys.exit(app.exec_())
